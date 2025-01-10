d5f7297d5773e52b106ea00eaf5ec87784e039ce
Boris Strahija <bstrahija@gmail.com>
2025-01-10 01:20:25 +0100

    News link to FB

--- resources/views/components/navigation/links.blade.php
+++ resources/views/components/navigation/links.blade.php
@@ -1,4 +1,4 @@
-<li><x-navigation.link href="{{ route('news') }}">Novosti</x-navigation.link></li>
+<li><x-navigation.link href="{{ \App\Services\Settings::get('general.facebook') }}" target="_blank">Novosti</x-navigation.link></li>
 <li><x-navigation.link href="{{ route('results') }}">Rezultati</x-navigation.link></li>
 <li><x-navigation.link href="{{ route('schedule') }}">Raspored</x-navigation.link></li>
 <li><x-navigation.link href="{{ route('teams') }}">Ekipe</x-navigation.link></li>
