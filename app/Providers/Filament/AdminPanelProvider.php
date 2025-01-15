a86f96c87ee4f0135df1d0c1f89eaff0e3d8afb4
Boris Strahija <bstrahija@gmail.com>
2025-01-15 13:36:09 +0100

    Add plugin to clear cache

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use CmsMulti\FilamentClearCache\FilamentClearCachePlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -37,6 +38,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->plugins([
                 \Schmeits\FilamentUmami\FilamentUmamiPlugin::make(),
+                FilamentClearCachePlugin::make(),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
