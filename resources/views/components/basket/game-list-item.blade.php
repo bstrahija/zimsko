481a527d4fc6517e651da8948b8744fe933e0f3e
Boris Strahija <bstrahija@gmail.com>
2025-01-20 22:54:00 +0100

    Fix layout issues

--- resources/views/components/basket/game-list-item.blade.php
+++ resources/views/components/basket/game-list-item.blade.php
@@ -1,5 +1,5 @@
 <div class="pb-4 mb-6 border-b">
-    <div class="mb-2 text-sm text-center uppercase font-roboto text-secondary">
+    <div class="mb-4 text-sm text-center uppercase font-roboto text-secondary">
         <span class="font-bold">
             @if ($game->round->type === 'finals')
                 🏆
@@ -13,25 +13,29 @@
     </div>
 
     <a href="{{ route('results.show', $game->slug) }}"
-        class="game-list__item grid grid-cols-[8%_30%_auto_30%_8%]  md:grid-cols-[10%_28%_auto_28%_10%] gap-3 font-condensed hover:scale-105 transition-all">
-        <div class="flex justify-center items-center"><img src="{{ $game->homeTeam->logo() }}" alt=""
-                class="max-h-8 rounded-full transition-transform duration-300 transform max-w-8 md:max-h-12 md:max-w-12 hover:scale-110"></div>
+        class="game-list__item grid grid-cols-[33%_auto_33%] max-w-full overflow-hidden md:grid-cols-[10%_28%_auto_28%_10%] gap-3 font-condensed hover:scale-105 transition-all">
+        <div class="flex flex-col gap-4 items-center text-center">
+            <div class="flex justify-center items-center"><img src="{{ $game->homeTeam->logo() }}" alt=""
+                    class="max-h-8 rounded-full transition-transform duration-300 transform max-w-8 md:max-h-12 md:max-w-12 hover:scale-110"></div>
 
-        <div class="flex items-center transition-colors duration-300 hover:text-primary">
-            <span class="overflow-hidden max-w-full whitespace-nowrap text-ellipsis">{{ $game->homeTeam->title }}</span>
+            <div class="flex items-center transition-colors duration-300 hover:text-primary">
+                <span class="overflow-hidden max-w-full">{{ $game->homeTeam->title }}</span>
+            </div>
         </div>
 
-        <div class="flex justify-center items-center text-xl font-bold font-condensed text-primary">
+        <div class="flex justify-center items-center text-lg font-bold md:text-xl font-condensed text-primary">
             <span class="{{ $game->home_score > $game->away_score ? 'text-primary' : 'text-gray-400' }}">{{ $game->home_score }}</span>
             <span class="mx-1">:</span>
             <span class="{{ $game->away_score > $game->home_score ? 'text-primary' : 'text-gray-400' }}">{{ $game->away_score }}</span>
         </div>
 
-        <div class="flex justify-end items-center transition-colors duration-300 hover:text-primary">
-            <span class="overflow-hidden max-w-full whitespace-nowrap text-ellipsis">{{ $game->awayTeam->title }}</span>
-        </div>
+        <div class="flex flex-col gap-4 justify-center items-center text-center">
+            <div class="flex justify-center items-center"><img src="{{ $game->awayTeam->logo() }}" alt=""
+                    class="max-h-8 rounded-full transition-transform duration-300 transform max-w-8 md:max-h-12 md:max-w-12 hover:scale-110"></div>
 
-        <div class="flex justify-center items-center"><img src="{{ $game->awayTeam->logo() }}" alt=""
-                class="max-h-8 rounded-full transition-transform duration-300 transform max-w-8 md:max-h-12 md:max-w-12 hover:scale-110"></div>
+            <div class="flex justify-end items-center transition-colors duration-300 hover:text-primary">
+                <span class="block overflow-hidden max-w-full">{{ $game->awayTeam->title }}</span>
+            </div>
+        </div>
     </a>
 </div>
