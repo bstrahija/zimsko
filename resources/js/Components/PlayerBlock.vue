320d120a7710c893c4abbe6e92f16a9a277a8eba
Boris Strahija <bstrahija@gmail.com>
2025-01-21 14:29:19 +0100

    Show fouls and points for players

--- resources/js/Components/PlayerBlock.vue
+++ resources/js/Components/PlayerBlock.vue
@@ -13,10 +13,18 @@ defineProps({
         class="overflow-hidden relative h-full text-center text-white rounded border shadow-sm transition-all pointer-events-auto hover:opacity-90 hover:shadow-lg player-block aspect-square"
         :class="{ 'active': active }">
         <div>
+            <div class="flex absolute top-1 left-1 z-40 justify-center items-center w-4 h-4 font-bold text-white rounded-full opacity-70 text-3xs bg-green-600/80">
+                {{ player.stats.score }}
+            </div>
+
+            <div class="flex absolute top-1 right-1 z-40 justify-center items-center w-4 h-4 font-bold text-white rounded-full opacity-70 text-3xs bg-red-600/80">
+                {{ player.stats.fouls }}
+            </div>
+
             <div class="z-30 font-mono text-sm font-bold shadow-lg md:text-lg lg:text-xl xl:text-3xl">
                 {{ player.number ? player.number : '-' }}
-
             </div>
+
             <small class="block relative z-20 font-medium text-center opacity-60 text-3xs lg:text-2xs xl:text-xs line-clamp-2">
                 {{ player.name }}
             </small>
