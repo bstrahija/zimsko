b99ce92518240be0bd8abbf7b786b1ff42fce5b4
Boris Strahija <bstrahija@gmail.com>
2025-01-24 11:24:48 +0100

    Reset

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vuePlugin from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/live.js', 'resources/css/filament/admin/theme.css'],
-            refresh: true,
-        }),
-        vuePlugin(),
-    ],
+    plugins: [tailwindcss()],
 });
