edd179c7675df4aa2f8fef1b9256a7def4fdfd56
Boris Strahija <bstrahija@gmail.com>
2025-03-11 10:16:19 +0100

    Change shirt

--- resources/views/livewire/merch/heading.blade.php
+++ resources/views/livewire/merch/heading.blade.php
@@ -1,6 +1,6 @@
 <div class="p-6 mb-8 bg-gray-100 rounded-lg shadow-sm">
     <h3 class="mb-4 text-xl font-semibold text-primary">Podržite Naš Turnir</h3>
-    <p class="mb-3 text-gray-700">Osigurajte si limited edition majicu i podržite naše prvenstvo donacijom 15 EUR za dry-fit majicu ili 30 EUR za hoodie.
+    <p class="mb-3 text-gray-700">Osigurajte si limited edition majicu i podržite naše prvenstvo donacijom 15 EUR za t-shirt majicu ili 30 EUR za hoodie.
     </p>
     <p class="text-sm italic text-gray-500">Sve donacije idu direktno za organizaciju i razvoj turnira.</p>
 </div>
