f364c20322e7e51598e6418326bfff03dab362c7
Boris Strahija <bstrahija@gmail.com>
2026-01-03 22:40:52 +0100

    Fixes

--- resources/views/components/teams/details.blade.php
+++ resources/views/components/teams/details.blade.php
@@ -1,6 +1,8 @@
-<a href="{{ $team->photo('original') }}" target="_blank" class="lightbox" data-pswp-width="{{ $team->photoSize('w') }}" data-pswp-height="{{ $team->photoSize('h') }}">
-    <img src="{{ $team->photo('original') }}" alt="{{ $team->title }}" class="w-full">
-</a>
+@if ($team->photo())
+    <a href="{{ $team->photo('original') }}" target="_blank" class="lightbox" data-pswp-width="{{ $team->photoSize('w') }}" data-pswp-height="{{ $team->photoSize('h') }}">
+        <img src="{{ $team->photo('original') }}" alt="{{ $team->title }}" class="w-full">
+    </a>
+@endif
 
 <table class="mt-8 w-full text-xs text-left">
     <tr class="border-b border-gray-200">
@@ -8,7 +10,7 @@
         <td class="py-2">{{ $team->gameCount() }}</td>
     </tr>
     <tr class="border-b border-gray-200">
-        <th class="py-2">Utakmica (Zimsko 2025):</th>
+        <th class="py-2">Utakmica (Zimsko 2026):</th>
         <td class="py-2">{{ $team->gameCountCurrent() }}</td>
     </tr>
     <tr class="border-b border-gray-200">
@@ -16,7 +18,7 @@
         <td class="py-2">{{ $team->pointsAverage() }}</td>
     </tr>
     <tr class="border-b border-gray-200">
-        <th class="py-2">Prosjek (Zimsko 2025):</th>
+        <th class="py-2">Prosjek (Zimsko 2026):</th>
         <td class="py-2">{{ $team->pointsAverageCurrent() }}</td>
     </tr>
 </table>
